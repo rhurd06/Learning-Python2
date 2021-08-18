c721bf07d93826ce0dc694023f02012d1f8efa3e
Robyn Noble <robynhurd09@gmail.com>
2021-08-18 16:00:21 -0500

    learning about initializing functions

--- cars.py
+++ cars.py
@@ -1,9 +1,12 @@
 class Car:
     runs = True
 
-    def start(self, name):
+    def __init__(self, name):
+        print("new car!")
         self.name = name
+
+    def start(self):
         if self.runs:
-            print(f"{name} car is started")
+            print(f"{self.name} car is started")
         else:
             print(f"{self.name} car is broken :(!")
