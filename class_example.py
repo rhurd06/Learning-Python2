ea96c9e4c2af0674fec593f0b7343215e1dbfaff
Robyn Noble <robynhurd09@gmail.com>
2021-08-19 16:05:33 -0500

    more practice with classes and inheritance

--- class_example.py
+++ class_example.py
@@ -1,7 +1,5 @@
 class Vehicle:
 
-    num_of_wheels = 4
-
     def __init__(self, make, model, fuel="gas"):
         self.make = make
         self.model = model
@@ -11,8 +9,40 @@ class Vehicle:
         return f"I drive {self.make} {self.model}. It runs on {self.fuel}."
 
 
+class Car(Vehicle):
+
+    number_of_wheels = 4
+
+
+class Truck(Vehicle):
+
+    number_of_wheels = 6
+
+    def __init__(self, make, model, fuel="diesel"):
+        super().__init__(make, model, fuel)
+
+
 daily = Vehicle("Subaru", "Crosstrek")
 print(daily)
 
-print("for Class", Vehicle.num_of_wheels)
-print("for Instance", daily.num_of_wheels)
+# print("for Class", Vehicle.number_of_wheels)
+# print("for Instance", daily.number_of_wheels)
+
+# daily.num_of_wheels = 3
+# print("for Class", Vehicle.num_of_wheels)
+# print("for Instance", daily.num_of_wheels)
+
+
+class GitHubRepo:
+
+    def __init__(self, name, language, num_stars):
+        self.name = name
+        self.langauge = language
+        self.num_stars = num_stars
+
+    def __str__(self):
+        return f"-> {self.name} is a {self.language} repo with {self.num_stars} stars."
+
+
+vue = GitHubRepo(name="Vue", language="JavaScript", num_stars=50000)
+print(vue)
