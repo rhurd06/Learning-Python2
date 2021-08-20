d2a854a9db91d15410e5dcacdb32e08584f405bd
Robyn Noble <robynhurd09@gmail.com>
2021-08-20 15:22:00 -0500

    continuing flask learning journey

--- hello_world.py
+++ hello_world.py
@@ -1,16 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 # to get it to run:
 # FLASK_APP= hello_world.py (filename) flask run
 
 # to set a new environment (for debugging & automatic reload)
-# FLASK_APP= hello_world.py FLASK_ENV=development
+# FLASK_APP=hello_world.py FLASK_ENV=development flask run
 
 
 @app.route("/")
 def hello_world():
-    return "Hello World!"
+    # return "Hello World!"
+    name = "Robyn"
+    return render_template("index.html", name=name)
 
 
 @app.route("/french")
